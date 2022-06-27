0f7ee74d5ef92664a765f692aeaed08b530cfcfb
hengyangji <hengyangji@tencent.com>
2022-06-27 16:34:31 +0800

    feat:提交插件项目

--- app/src/main/java/com/sun/replugin/MainActivity.kt
+++ app/src/main/java/com/sun/replugin/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.sun.replugin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.qihoo360.replugin.RePlugin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.start_plugin_btn).setOnClickListener {
+
+            // 刻意以“包名”来打开
+            RePlugin.startActivity(
+                this@MainActivity,
+                RePlugin.createIntent(
+                    "com.sun.plugin",
+                    "com.sun.plugin.MainActivity"
+                )
+            )
+        }
     }
 }
--- app/src/main/java/com/sun/replugin/SunApplication.kt
+++ app/src/main/java/com/sun/replugin/SunApplication.kt
@@ -1,13 +1,13 @@
 package com.sun.replugin
 
-import android.app.Application
 import android.content.Context
 import android.util.Log
+import com.qihoo360.replugin.RePluginApplication
 
 /**
  * Created by hengyangji on 2022/6/20
  */
-class SunApplication : Application() {
+class SunApplication : RePluginApplication() {
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
         Log.i("SunApplication", "attachBaseContext")

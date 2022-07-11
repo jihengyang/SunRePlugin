8193eedd3b7a76a06bf7f571bea918ba065d73eb
hengyangji <hengyangji@tencent.com>
2022-07-11 09:31:25 +0800

    feat:完善插件项目

--- plugin/src/main/java/com/sun/plugin/MainActivity.java
+++ plugin/src/main/java/com/sun/plugin/MainActivity.java
@@ -2,12 +2,15 @@ package com.sun.plugin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import com.qihoo360.replugin.loader.a.PluginActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends PluginActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plugin_main);
+        Log.i("MyPlugin", "classLoader:" + getClassLoader());
     }
 }
